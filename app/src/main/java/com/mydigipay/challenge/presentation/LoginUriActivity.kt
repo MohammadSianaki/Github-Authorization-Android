3c4e29fa64eee5f63dcb0335998cc8693619f5ad
Mohammad Sianaki <m.rsianaki@yahoo.com>
2020-01-29 20:54:27 +0330

    Change ui updating to databinding

--- app/src/main/java/com/mydigipay/challenge/presentation/LoginUriActivity.kt
+++ app/src/main/java/com/mydigipay/challenge/presentation/LoginUriActivity.kt
@@ -3,18 +3,23 @@ package com.mydigipay.challenge.presentation
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.observe
+import com.github.mohammadsianaki.core.model.ErrorHolder
+import com.github.mohammadsianaki.core.model.ResourcesState
 import com.mydigipay.challenge.github.R
+import com.mydigipay.challenge.github.databinding.LoginUriActivityBinding
 import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.getViewModel
 
 
 class LoginUriActivity : AppCompatActivity() {
     private lateinit var viewModel: LoginUriViewModel
+    private lateinit var binding: LoginUriActivityBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.login_uri_activity)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 
     override fun onResume() {
@@ -28,8 +33,32 @@ class LoginUriActivity : AppCompatActivity() {
                 }
             } ?: run { finish() }
         }
-        viewModel.getLiveData().observe(this) {
+        viewModel.getLiveData().observe(this) { resource ->
+            when (resource.resourcesState) {
+                ResourcesState.Success -> {
+                    handleData(resource.data)
+                }
+                ResourcesState.Loading -> {
+                    handleLoading()
+                }
+                ResourcesState.Failure -> {
+                    handleFailure(resource.failure)
+                }
+            }
+        }
+    }
 
+    private fun handleFailure(failure: ErrorHolder?) {
+        // todo : handle this situation
+    }
+
+    private fun handleLoading() {
+        // todo : handle this situation
+    }
+
+    private fun handleData(data: AccessTokenModel?) {
+        data?.let {
+            binding.itemData = it
         }
     }
 
